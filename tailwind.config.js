c4a6eecee7dd28f4ec1dbf973efb0cafccc680f0
pixelzee3 <bpixelzee@gmail.com>
2022-02-08 18:40:57 +0700

    Define theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  mode: "jit",
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  mode: 'jit',
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      cyan: 'hsl(178, 100%, 50%)',
+      'almost-very-dark-blue': 'hsl(216, 50%, 16%)',
+      'very-dark-blue': 'hsl(217, 54%, 11%)',
+    },
   },
   plugins: [],
-}
+};
