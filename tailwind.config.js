35dba631a8b52f89711cac300310442848b16582
pixelzee3 <bpixelzee@gmail.com>
2022-02-09 22:25:24 +0700

    Define typography.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,16 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      'soft-blue': 'hsl(215, 51%, 70%)',
-      cyan: 'hsl(178, 100%, 50%)',
-      'almost-very-dark-blue': 'hsl(216, 50%, 16%)',
-      'very-dark-blue': 'hsl(217, 54%, 11%)',
+    extend: {
+      colors: {
+        'soft-blue': 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+        'almost-very-dark-blue': 'hsl(216, 50%, 16%)',
+        'very-dark-blue': 'hsl(217, 54%, 11%)',
+      },
+    },
+    fontFamily: {
+      'sans': ['Outfit', 'sans-serif']
     },
   },
   plugins: [],
